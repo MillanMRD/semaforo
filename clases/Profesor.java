16fbeb092d72d137aa8620720b4215659291e2b4
MillanMRD <millanmrd@gmail.com>
2022-03-07 08:31:01 +0100

    Comentario hola

---
+++ clases/Profesor.java
@@ -0,0 +1,18 @@
+package clases;
+
+public class Profesor extends Persona {
+	public Profesor() {
+	} // Constructor
+
+	// Hace la pregunta al estudiante sobre el color
+	public String preguntacolor() {
+
+		Estudiante alumno = new Estudiante();
+		String colorRec = alumno.preguntacolor();
+		return colorRec;
+	}
+}
+/**
+ * 
+ * dbsdgbdbsdggn
+ */
---
+++ principal/Clasecolor.java
@@ -0,0 +1,12 @@
+package principal;
+
+import clases.Profesor;
+
+// Clase color, el profesor pregunta a un alumno por un color entre (rojo, amarillo y verde)
+public class Clasecolor {
+	public static void main(String[] args) {
+		Profesor teacher = new Profesor();
+		String color = teacher.preguntacolor();
+		System.out.println("La respuesta recibida es:" + color);
+	}
+}
